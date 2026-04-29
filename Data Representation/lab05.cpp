43d3b0280a189b55efae4cb8fef14b21fdc081e9
jaustrylle <sanren.reese@gmail.com>
2026-04-28 22:04:40 -0500

    Add files via upload

---
+++ Data Representation/lab05.cpp
@@ -0,0 +1,45 @@
+// Serena Reese - CS 3304 - Lab 05
+
+#include <iostream>
+#include <iomanip>
+#include <climits>
+#include <string>
+#include <vector>
+
+#include <type_traits>	// prepare bit mask
+
+using namespace std;
+
+/* NOTES:
+1. receives an integral parameter
+2. returns a string (internal binary rep. of input, aka two's complement)
+3. must be portable (works no matter what machine is being used)
+4. remember to use make clean command
+*/
+
+template <typename T>
+
+string int2binary(T num){
+	
+	// type safety check
+	static_assert(is_integral<T>::value, "int2binary requires an integral type.");
+	
+	// fail-safe
+	using U = typename make_unsigned<T>::type;
+	
+	// get # of bits
+	const int bits = sizeof(T) * CHAR_BIT;
+	
+	// convert to corresponding unsigned type
+	U value = static_cast<U>(num);
+
+	// extract bits from MSB to LSB, masking bits (avoid UB, handle negatives)
+	string binary;
+	binary.reserve(bits);
+	
+	for(int i = bits-1; i >= 0; --i){
+		binary.push_back((value & (U(1) << i)) ? '1' : '0');
+	}
+	
+	return binary;
+}
